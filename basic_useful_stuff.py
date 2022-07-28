1df24eafdd852665e5165668c2245cd5477274d8
barbora-anna <90620914+barbora-anna@users.noreply.github.com>
2022-07-28 22:21:55 +0200

    Update basic_useful_stuff.py

--- basic_useful_stuff.py
+++ basic_useful_stuff.py
@@ -1,28 +1,31 @@
 import os
 
-## Creates a new folder - if not existent already
-# If a folder exists, exception is raised
 def create_folder(my_path):
+  '''Create a new folder'''
   if not os.path.isdir(my_path):
     os.mkdir(my_path)
   else:
     raise Exception('Dir already exists')
     
 
-## Flattens a list of lists
 def flatten(a_list):
-  flat_list = [item for sublist in a_list for item in sublist]
-  return flat_list
+  '''Flatten a list of lists'''
+  return [item for sublist in a_list for item in sublist]
+  
 
-
-## Multiplies items in a given list 
-# ex.: a_list=[a, b, c], n=2 --> [a, a, b, b, c, c]
 def multiply_items(a_list, n):
-  multiplied_items = flatten([[i]*n for i in a_list])
-  return multiplied_items
+  '''Multiply items in a list: a_list=[a, b, c], n=2 --> [a, a, b, b, c, c]'''
+  return flatten([[i]*n for i in a_list])
 
-def export_list(my_list, new_filename):
-  textfile = open(new_filename, "w")
-  for element in my_list:
-      textfile.write(element + "\n")
-  textfile.close()
+
+def export_list(my_list, new_filename, sep='\n'):
+  '''Export a list. Separator customizable, \n by default'''
+  if not os.path.isfile(new_filename):
+    new_file = open(new_filename, "w")
+    for item in my_list:
+        new_file.write(item + sep)
+    new_file.close()
+  else:
+    raise Exception('File already exists')
+
+
